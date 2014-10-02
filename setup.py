a63cdc6b6f5bb63790ee4405eb69c55a3ef86b64
Dale Jung <dale@dalejung.com>
2014-10-02 12:37:50 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+DISTNAME='earthdragon'
+FULLVERSION='0.1'
+
+setup(
+    name=DISTNAME,
+    version=FULLVERSION,
+    packages=['earthdragon'],
+    entry_points={
+    }
+      )
