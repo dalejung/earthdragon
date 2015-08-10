68821c60738ae8acb9ccdd07d65de68f97a21dc0
Dale Jung <dale@dalejung.com>
2015-08-10 15:15:53 -0400

    Moved metaclass from hansel

---
+++ earthdragon/meta.py
@@ -0,0 +1,66 @@
+"""
+Meta tools for class construction.
+"""
+from collections import OrderedDict
+import inspect
+import ctypes
+
+def reload_locals(frame):
+    ctypes.pythonapi.PyFrame_LocalsToFast(ctypes.py_object(frame), ctypes.c_int(1))
+
+class MetaDict(OrderedDict):
+    def __init__(self, setitem_handler):
+        self.setitem_handler = setitem_handler
+        self.classdict = ClassDict(self)
+        super().__init__()
+
+    def __setitem__(self, key, value):
+        ret = self.setitem_handler(key, value, self.classdict)
+        if ret is False:
+            return
+        super().__setitem__(key, value)
+
+class ClassDict:
+    """ simple obj to limit interface to MetaDict """
+    def __init__(self, dct):
+        self.dct = dct
+
+    def __setitem__(self, key, value):
+        super(MetaDict, self.dct).__setitem__(key, value)
+
+    def __getitem__(self, key):
+        return self.dct[key]
+
+    def __contains__(self, key):
+        return key in self.dct
+
+    def __delitem__(self, key):
+        del self.dct[key]
+
+    def keys(self):
+        return self.dct.keys()
+
+class MetaMeta(type):
+    @classmethod
+    def __prepare__(mcl, name, bases):
+        mdict = MetaDict(mcl.setitem_handler)
+        return mdict
+
+    def setitem_handler(key, value, scope):
+        # raise NotImplementedError?
+        pass
+
+def mro(dct, bases, name):
+    class_dicts = [base.__dict__ for base in bases] + [dct]
+    for cdict in class_dicts:
+        if name in cdict:
+            return cdict[name]
+
+def derived_methods(self):
+    pass
+    # TODO
+    # it's a common idiom for a class to return attribute dynamic.
+    # however these attributes are usually derived from some fixed
+    # data like a list of columns or indexes.
+    # so we should be able to more solidly define these items and also
+    # easily add support with __dir__
---
+++ earthdragon/test/test_meta.py
@@ -0,0 +1,69 @@
+import nose.tools as nt
+
+from ..meta import MetaMeta
+
+def middle_matcher(start, end):
+    def _match(key):
+        if not (key.startswith(start) and key.endswith(end)):
+            return False
+        middle = key[len(start):-len(end)]
+        return middle
+    return _match
+
+set_to_match = middle_matcher("set_", "_to")
+
+def test_scope_add():
+    class TestMeta(MetaMeta):
+        def setitem_handler(key, value, scope):
+            mid_name = set_to_match(key)
+            if not mid_name:
+                return
+            scope[mid_name] = value
+
+    # first case. bob is undefined
+    with nt.assert_raises(NameError):
+        class TestClass(metaclass=TestMeta):
+            l = bob
+            dale = 1
+
+    # using TestMeta and setitem_handler, bob gets added to ns. no error
+    class TestClass(metaclass=TestMeta):
+        set_bob_to = 'whee'
+        l = bob
+        dale = 1
+
+    nt.assert_equal(TestClass.l, 'whee')
+    # set_bob_to is not added to class. Just a directive
+    nt.assert_true(hasattr(TestClass, 'set_bob_to'))
+
+def test_scope_add_temporal():
+    class TestMeta2(MetaMeta):
+        def setitem_handler(key, value, scope):
+            mid_name = set_to_match(key)
+            if not mid_name:
+                return
+            scope[mid_name] = value
+            return False # don't add directive to class
+
+    # using TestMeta and setitem_handler, bob gets added to ns. no error
+    class TestClass2(metaclass=TestMeta2):
+        set_bob_to = 'whee'
+        l = bob
+        dale = 1
+    nt.assert_false(hasattr(TestClass2, 'set_bob_to'))
+
+class TestMeta2(MetaMeta):
+    def setitem_handler(key, value, scope):
+        import inspect
+        frames = inspect.stack()
+        mid_name = set_to_match(key)
+        if not mid_name:
+            return
+        scope[mid_name] = value
+        return False # don't add directive to class
+
+# using TestMeta and setitem_handler, bob gets added to ns. no error
+class TestClass2(metaclass=TestMeta2):
+    set_bob_to = 'whee'
+    l = bob
+    dale = 1
