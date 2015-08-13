010f9d30f39139c809b02175c0f9b854199c983e
Dale Jung <dale@dalejung.com>
2015-08-12 23:42:43 -0400

    placeholder NavelMeta

---
+++ earthdragon/navel/__init__.py
@@ -0,0 +1,1 @@
+from .navel import Navel, NavelMeta
--- earthdragon/navel/navel.py
+++ earthdragon/navel/navel.py
@@ -1,6 +1,9 @@
-from ..feature import FeatureBase, features
+from ..feature import FeatureBase, features, FeatureMeta
 from .lockable import Lockable
 
+class NavelMeta(FeatureMeta):
+    pass
+
 @features(Lockable)
-class Navel(FeatureBase):
+class Navel(FeatureBase, metaclass=FeatureMeta):
     pass
