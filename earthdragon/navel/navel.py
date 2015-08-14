cf17d0ab57813d1d5daad7174944671113d62fb1
Dale Jung <dale@dalejung.com>
2015-08-14 00:05:56 -0400

    gather the typelets, moved from hansel

--- earthdragon/navel/navel.py
+++ earthdragon/navel/navel.py
@@ -1,8 +1,13 @@
 from ..feature import FeatureBase, features, FeatureMeta
 from .lockable import Lockable
+from ..typelet import Typelet, gather_typelets, typelet_repr
 
 class NavelMeta(FeatureMeta):
-    pass
+    def __new__(cls, name, bases, dct):
+        if bases:
+            typelets = gather_typelets(dct, bases)
+            dct['_earthdragon_typelets'] = typelets
+        return super().__new__(cls, name, bases, dct)
 
 @features(Lockable)
 class Navel(FeatureBase, metaclass=FeatureMeta):
