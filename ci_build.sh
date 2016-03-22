b1b55b1df556de19d511f3c58c944d728f355950
Dale Jung <dale@dalejung.com>
2016-03-22 13:26:23 -0400

    CI: changed certain deps to run off of master

--- ci_build.sh
+++ ci_build.sh
@@ -2,6 +2,6 @@
 
 pip install pandas
 pip install numpy
-pip install git+https://github.com/berkerpeksag/astor
-pip install git+https://github.com/dalejung/asttools
+pip install -e git+https://github.com/berkerpeksag/astor#egg=astor
+pip install -e git+https://github.com/dalejung/asttools#egg=asttools
 pip install .
