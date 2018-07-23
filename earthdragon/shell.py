0847b39c3a2faf9ef2aa5161524b3b2fae5f647f
Dale Jung <dale@dalejung.com>
2018-07-22 23:48:38 -0400

    pushd

--- earthdragon/shell.py
+++ earthdragon/shell.py
@@ -1,4 +1,6 @@
 import subprocess
+import os
+from contextlib import contextmanager
 
 def run(cmd, **kwargs):
     if kwargs:
@@ -6,3 +8,11 @@ def run(cmd, **kwargs):
     out = subprocess.check_output(cmd, shell=True)
     return out.decode('utf-8')
 
+@contextmanager
+def pushd(new_dir):
+    previous_dir = os.getcwd()
+    os.chdir(new_dir)
+    try:
+        yield
+    finally:
+        os.chdir(previous_dir)
