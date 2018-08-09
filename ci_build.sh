70c14bea75d724df403b32d73292562f24859457
Dale Jung <dale@dalejung.com>
2018-08-09 15:07:53 -0400

    fix ci

--- ci_build.sh
+++ ci_build.sh
@@ -2,6 +2,7 @@
 
 pip install pandas
 pip install numpy
+pip install typeguard
 pip install -e git+https://github.com/berkerpeksag/astor#egg=astor
 pip install -e git+https://github.com/dalejung/asttools#egg=asttools
 pip install .
