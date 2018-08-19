fd41e78f90f581c216d12b6e2af015d76aa5cde1
Dale Jung <dale@dalejung.com>
2018-08-19 03:50:52 -0400

    hi

--- ci_build.sh
+++ ci_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-pip install -e git+https://github.com/pandas-dev/pandas.git#egg=pandas
 pip install numpy
+pip install git+https://github.com/pandas-dev/pandas.git#egg=pandas
 pip install typeguard
 pip install -e git+https://github.com/berkerpeksag/astor#egg=astor
 pip install -e git+https://github.com/dalejung/asttools#egg=asttools
