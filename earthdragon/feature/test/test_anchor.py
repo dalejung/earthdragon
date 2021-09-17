9a078d9ad2527bc4c3aef741270b5bd64e96888a
Dale Jung <dale@dalejung.com>
2021-09-17 15:10:12 -0400

    remove notest

--- earthdragon/feature/test/test_anchor.py
+++ earthdragon/feature/test/test_anchor.py
@@ -1,6 +1,6 @@
 from collections import OrderedDict, defaultdict
 from itertools import groupby
-import nose.tools as nt
+import pytest
 
 from ..attr import Attr
 from ..anchor import (
@@ -14,6 +14,7 @@ from ..anchor import (
     AnchorMeta
 )
 
+
 def test_add_to_attr():
     @hook('hello')
     def fake_hook():
@@ -21,19 +22,24 @@ def test_add_to_attr():
 
     hello = Attr()
     add_to_attr(hello, fake_hook)
-    nt.assert_is(hello.hooks[0], fake_hook.func)
+    assert hello.hooks[0] is fake_hook.func
 
 
 @hook('hello')
 def hello_hook(): yield
 
+
 @hook('hello')
 def hello_hook2(): yield
+
 
 @hook('api_method')
 def api_hook(): yield
 
-api_method = lambda x: x
+
+def api_method(x):
+    return x
+
 
 def test_aggregate_anchor_func():
     dct = OrderedDict()
@@ -43,23 +49,24 @@ def test_aggregate_anchor_func():
     dct['api_hook'] = api_hook
 
     update = _aggregate_anchor_funcs(dct)
-    nt.assert_is_instance(update['hello'], Attr)
-    nt.assert_is_instance(update['api_method'], Attr)
+    assert isinstance(update['hello'], Attr)
+    assert isinstance(update['api_method'], Attr)
 
     attr = update['hello']
-    nt.assert_is(attr.orig_func, None)
-    nt.assert_list_equal(attr.hooks, [hello_hook.func, hello_hook2.func])
+    assert attr.orig_func is None
+    assert attr.hooks == [hello_hook.func, hello_hook2.func]
 
     attr = update['api_method']
-    nt.assert_is(attr.orig_func, api_method)
-    nt.assert_list_equal(attr.hooks, [api_hook.func])
+    assert attr.orig_func is api_method
+    assert attr.hooks == [api_hook.func]
+
 
 def test_aggregate_anchor_func_error():
     dct = OrderedDict()
     dct['api_hook'] = api_hook
     dct['api_method'] = Attr(api_method)
 
-    with nt.assert_raises(AnchorFuncError):
+    with pytest.raises(AnchorFuncError):
         update = _aggregate_anchor_funcs(dct)
 
 
@@ -98,16 +105,17 @@ def test_anchor_meta():
             yield
 
     b2 = Blah2(3)
-    nt.assert_not_in('init_args', b2.__dict__)
+    assert 'init_args' not in b2.__dict__
     ret = b2.shout('hi')
-    nt.assert_equal(ret, 'hihi')
+    assert ret == 'hihi'
 
     b3 = Blah3(3)
-    nt.assert_list_equal(b3.init_args, [3])
+    assert b3.init_args == [3]
     ret = b3.shout('hi')
-    nt.assert_equal(ret, 'hihi')
-    nt.assert_list_equal(b3.logs, ['hi', 'hi2'])
-    nt.assert_list_equal(b3.shout.hooks, [Blah2.log.func, Blah3.log3.func])
+    assert ret == 'hihi'
+    assert b2.logs == ['hi', 'hi2']
+    assert b3.shout.hooks == [Blah2.log.func, Blah3.log3.func]
+
 
 def test_anchor_meta_single():
     """ test using attr and hooks on a single class """
@@ -127,16 +135,15 @@ def test_anchor_meta_single():
         def first_letter(what):
             return what[0]
 
-
     obj = Single()
     ret = obj.hooked('word')
-    nt.assert_equal(ret, 'w')
-    nt.assert_equal(obj.pre_what, 'word')
-    nt.assert_equal(obj.post_what, 'w')
+    assert ret == 'w'
+    assert obj.pre_what == 'word'
+    assert obj.post_what == 'w'
 
 
 def test_anchor_meta_incorrect_order():
-    with nt.assert_raises(AnchorFuncError):
+    with pytest.raises(AnchorFuncError):
         class SingleError(metaclass=AnchorMeta):
             @hook('hooked')
             def hookhook(self, what):
