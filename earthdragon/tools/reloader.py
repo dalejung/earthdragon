8d09ffc35c1d0338e5e19eac4a6e17439d41dace
Dale Jung <dale@dalejung.com>
2021-11-18 17:54:11 -0500

    wip with reloading namespace

--- earthdragon/tools/reloader.py
+++ earthdragon/tools/reloader.py
@@ -1,5 +1,7 @@
+import sys
 import types
 import importlib
+import inspect
 
 
 def reimport(obj):
@@ -30,3 +32,38 @@ def reimport_module(module):
         module = importlib.import_module(module)
     module = importlib.reload(module)
     return module
+
+
+def get_existing_module(module):
+    if isinstance(module, types.ModuleType):
+        module = module.__name__
+
+    return sys.modules.get(module, None)
+
+
+def reimport_ns(module, depth=1):
+    """
+    TODO: Basically want the ability to specify a module *after* import
+    statements and have it search out and replace references that:
+
+    1. Were defined in module
+    2. Were imported from that module.
+
+    So #2 is a bit open to how to accomplish. I could just check `module` attrs
+    match any scope objects. That still isn't 100% since that won't track the
+    exact `from mod import X` since we might've imported from a different
+    module. Could also parse the python to find the references.
+    """
+    existing_module = get_existing_module(module)
+    module = reimport_module(module)
+    module_path = module.__name__
+
+    frame = inspect.stack()[depth].frame
+    scope = frame.f_locals
+
+    direct_match = {}
+
+    for k, v in scope.items():
+        if getattr(v, '__module__', False) == module_path:
+            direct_match[k] = getattr(module, k)
+            continue
